6e82176612b2ebc8cb83de63e44834dc63c07bba
Don Brower <dbrower@nd.edu>
2014-03-28 14:36:58 -0400

    Update roster for 2014

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 
 ./make_leaderboard.py
 target=/Library/WebServer/Documents/
-files="leaderboard.css sjcpl_logo.jpg"
+files="leaderboard.css sjcpl_logo_bw.png"
 sudo cp leaderboard.html $target/index.html
 sudo cp $files $target
