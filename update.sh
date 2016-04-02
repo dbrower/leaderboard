b851a031f3a57b59ccb1ca8e1aabe9c0c315fb0f
Don Brower <dbrower@nd.edu>
2016-04-02 14:56:00 -0400

    Updates for 2016 trivia

--- update.sh
+++ update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 ./make_leaderboard.py
-target=/Library/WebServer/Documents/
+# for system apache
+#-target=/Library/WebServer/Documents/
+# for brew nginx
+target=/usr/local/var/www
 files="leaderboard.css sjcpl_logo_bw.png"
 sudo cp leaderboard.html $target/index.html
 sudo cp $files $target
